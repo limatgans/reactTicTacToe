e215510cc387532f7afbd659f5e70c704e39d7a8
limatgans <limatgans@gmail.com>
2019-08-19 15:15:44 +0530

    [feat] Adding Time Travel

--- react-tic-tac-toe/src/Board.js
+++ react-tic-tac-toe/src/Board.js
@@ -8,50 +8,38 @@ import calculateWinner from './utils/calculateWinner'
 
 export default class Board extends React.Component {
     
-    constructor(props) {
-        super(props)
-        this.state = {
-            squares: Array(9).fill(null),
-            isXNext: true
-        }
-    }
+    // constructor(props) {
+    //     super(props)
+    //     this.state = {
+    //         squares: Array(9).fill(null),
+    //         isXNext: true
+    //     }
+    // }
     
 	renderSquare(i) {
         return (
             <Square 
-                value={this.state.squares[i]}
-                onClick={()=> this.handleClick(i)} 
+            value={this.props.squares[i]}
+            onClick={() => this.props.onClick(i)}
             />
         );
     }
 
     
     handleClick(index) {
-        let squares = [...this.state.squares]
+        let squares = [...this.props.squares]
         if (calculateWinner(squares) || squares[index]) {
             return;
         }
-        squares[index] = this.state.isXNext? 'X' : 'O'
-        const isXNext = !this.state.isXNext
+        squares[index] = this.props.isXNext? 'X' : 'O'
+        const isXNext = !this.props.isXNext
         this.setState({squares, isXNext})
     }
 
 	render() {
-        let status
-        const isTied = (squares=[]) => squares.every((square) => square !== null)
-        const winner = calculateWinner(this.state.squares)
-        
-        if (winner) {
-            status = `Winner is: ${winner}`
-        } else if( isTied(this.state.squares) ) {
-            status = `Matched is Tied`;
-        } else {
-            status = `Player is: ${this.state.isXNext? 'X' : 'O'}`;
-        }
 
 		const board = (
 			<div>
-				<div className = "status">{status}</div>
 				<div className = "board-row">
                     {this.renderSquare(0)}
 					{this.renderSquare(1)}
--- react-tic-tac-toe/src/Game.js
+++ react-tic-tac-toe/src/Game.js
@@ -3,16 +3,91 @@ import React from 'react';
 // Importing Other Classses
 import Board from './Board.js'
 
+// Importing Utils
+import calculateWinner from './utils/calculateWinner'
+
 export default class Game extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+		  history: [{
+			squares: Array(9).fill(null),
+		  }],
+		  stepNumber: 0,
+		  xIsNext: true,
+		};
+	}
+
+	handleClick(i) {
+		const history = this.state.history.slice(0, this.state.stepNumber + 1);		
+		const current = history[history.length - 1];
+		const squares = current.squares.slice();
+		if (calculateWinner(squares) || squares[i]) {
+		  return;
+		}
+		squares[i] = this.state.xIsNext ? 'X' : 'O';
+		this.setState({
+		  history: history.concat([{
+			squares: squares,
+		  }]),
+		  stepNumber: history.length,
+		  xIsNext: !this.state.xIsNext,
+		});
+	}
+
+	jumpTo(step) {
+		this.setState({
+		  stepNumber: step,
+		  xIsNext: (step % 2) === 0,
+		});
+	}
+
 	render() {
-		const history = 'Start';
+		const history = this.state.history;
+		const current = history[this.state.stepNumber];
+		const winner = calculateWinner(current.squares);
+		let status;
+
+		// Function for caluculating whether Match is drawn
+		const isTied = (squares=[]) => squares.every((square) => square !== null)
+		const isSquaresNull =current.squares.findIndex((value) => value === null) === -1
+		
+		// Changing Status
+		if (winner) {
+            status = `Winner is: ${winner}`
+        } else if( isTied(current.squares) && isSquaresNull) {
+            status = `Matched is Tied`;
+        } else {
+            status = `Player is: ${this.state.xIsNext? 'X' : 'O'}`;
+		}
+
+		// Loading Moves based on history
+		const moves = history.map((step, move) => {
+			const desc = move ?
+			  'Go to move #' + move :
+			  'Go to game start';
+			return (
+				<li key={move}>
+					<button onClick={() => this.jumpTo(move)}>{desc}</button>
+			  	</li>
+			);
+		});
+	
+		// Rendering Game
 		const game = (
 			<div className = "game">
 				<div className = "gameboard">
-					<Board />
+					<Board 
+            			squares={current.squares}
+            			onClick={(i) => this.handleClick(i)}
+					/>
 				</div>
 				<div className = "game-history">
-					{history}
+					<div className = "status">
+						{status}
+					</div>
+					<ol>{moves}</ol>
 				</div>
 			</div>
 		)
